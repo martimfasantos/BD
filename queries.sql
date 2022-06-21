5819b2c2b2b05e6a351b7688637722be377467ab
martimfasantos <martimfasantos@gmail.com>
2022-06-21 14:20:16 +0100

    populate adjusted and queries done

---
+++ queries.sql
@@ -0,0 +1,59 @@
+------------------------------------------------------------------
+-- Qual o nome do retalhista (ou retalhistas) responsáveis pela -- 
+--          reposição do maior número de categorias?            --
+------------------------------------------------------------------
+
+SELECT nome
+FROM responsavel_por NATURAL JOIN retalhista
+GROUP BY nome 
+HAVING COUNT(DISTINCT nome_cat) >= ALL ( 
+        SELECT COUNT(DISTINCT nome_cat) 
+        FROM responsavel_por
+        GROUP BY tin );
+
+-- ## Expected: Tio Ze Manel
+        
+----------------------------------------------------------------        
+-- Qual o nome do ou dos retalhistas que são responsáveis por --
+--               todas as categorias simples?                 --
+----------------------------------------------------------------
+
+SELECT nome
+FROM responsavel_por NATURAL JOIN retalhista
+GROUP BY nome 
+HAVING COUNT(DISTINCT nome_cat) = ( 
+        SELECT COUNT(*) 
+        FROM categoria_simples );
+
+-- ## Expected: Tio Ze Manel
+
+-------------------------------------------------------
+-- Quais os produtos (ean) que nunca foram repostos? --
+-------------------------------------------------------
+
+SELECT ean
+FROM produto
+WHERE ean NOT IN (SELECT ean FROM evento_reposicao);
+
+-- ## Expected: 2662602572197 -> Pastel de Nata
+--              4832423897296 -> Leite UCAL Chocolate
+
+
+------------------------------------------------------------
+-- Quais os produtos (ean) que foram repostos sempre pelo --
+--                  mesmo retalhista?                     --
+------------------------------------------------------------
+
+SELECT ean
+FROM evento_reposicao
+GROUP BY ean
+HAVING COUNT(DISTINCT tin) = 1;
+
+-- ## Expected: 2662602572197 -> Pastel de Nata
+--              3274673078338 -> Red Bull
+--              4040298751684 -> Lays Presunto
+--              4832423897296 -> Leite UCAL Chocolate
+--              7499448750431 -> Perna de frango
+--              8601139975576 -> Hamburguer
+
+
