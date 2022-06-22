b3974a9d2b812d9c3c2ed643120524ff6cc772cd
diogo4256 <diogo.adegas@gmail.com>
2022-06-22 10:07:42 +0100

    view and olap Queries - V2

--- olap_queries.sql
+++ olap_queries.sql
@@ -2,24 +2,18 @@
 
 SELECT dia_semana, concelho, SUM(unidades) AS total_vendidos
 FROM vendas
-WHERE 
-	(ano > EXTRACT(YEAR FROM TIMESTAMP 'input1'
-	AND   mes > EXTRACT(MONTH FROM TIMESTAMP 'input1'
-	AND   dia_mes > EXTRACT(DAY FROM TIMESTAMP 'input1') 
-
-	AND
-
-	(ano < EXTRACT(YEAR FROM TIMESTAMP 'input2'
-	AND   mes < EXTRACT(MONTH FROM TIMESTAMP 'input2'
-	AND   dia_mes < EXTRACT(DAY FROM TIMESTAMP 'input2')
-
 GROUP BY
 	GROUPING SETS ( (dia_semana), (concelho), () )
+HAVING 
+    "11/10/2005" < CONCAT( CAST(dia_mes AS VARCHAR), "/", CAST(mes AS VARCHAR), "/", CAST(ano AS VARCHAR) ) AS data_reposicao
+    AND
+    data_reposicao < "21/07/2021"
 
 ----------------------------------------------Query 2----------------------------------------------
 
 SELECT concelho, cat, dia_semana, SUM(unidades) AS total_vendidos
-FROM
-WHERE (distrito = 'Lisboa')
+FROM vendas
+WHERE 
+    (distrito = 'Lisboa')
 GROUP BY
 	GROUPING SETS ( (concelho), (cat), (dia_semana), () )
--- view.sql
+++ view.sql
@@ -2,14 +2,13 @@
 
 CREATE VIEW vendas(ean, cat, ano, trimestre, mes, dia_mes, dia_semana, distrito, concelho, unidades) AS
 SELECT p.ean, c.nome as cat,
-        dt.ano, dt.trimestre, dt.mes, dt.dia_mes, dt.dia_semana,
+        EXTRACT(YEAR FROM er.instante) AS ano,
+        EXTRACT(QUARTER FROM er.instante) AS trimestre,
+        EXTRACT(MONTH FROM er.instante) AS mes,
+        EXTRACT(DAY FROM er.instante) AS dia_mes,
+        EXTRACT(DOW FROM er.instante) AS dia_semana,
         pr.distrito, pr.concelho, er.unidades
-FROM produto AS p, categoria AS c,
-        (SELECT
-        EXTRACT(YEAR FROM evento_reposicao.instante) AS ano,
-        EXTRACT(QUARTER FROM evento_reposicao.instante) AS trimestre,
-        EXTRACT(MONTH FROM evento_reposicao.instante) AS mes,
-        EXTRACT(DAY FROM evento_reposicao.instante) AS dia_mes,
-        EXTRACT(DOW FROM evento_reposicao.instante) AS dia_semana
-        FROM evento_reposicao) AS dt,
-        ponto_de_retalho AS pr, evento_reposicao AS er;
+FROM produto AS p, 
+     categoria AS c,
+     ponto_de_retalho AS pr, 
+     evento_reposicao AS er;
